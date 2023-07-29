e121c68a00e06b4633c435c87dfd2fc06187b973
GitHub Action <action@github.com>
2023-07-29 19:49:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/daym/ideasqlplugin/MyBundle.kt
+++ src/main/kotlin/com/github/daym/ideasqlplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.daym.ideasqlplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/daym/ideasqlplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/daym/ideasqlplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.daym.ideasqlplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
